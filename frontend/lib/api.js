0fd1682d8e13437bba6dca2aab25f38e463f497c
vsh1h <vsh1h@users.noreply.github.com>
2026-01-20 17:50:35 +0530

    bug:added backend url

--- frontend/lib/api.js
+++ frontend/lib/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = 'https://freightshare.onrender.com/api';
+const API_BASE_URL = 'https://freightshare-5f2y.onrender.com/api';
 
 export const api=axios.create({
   baseURL:API_BASE_URL
